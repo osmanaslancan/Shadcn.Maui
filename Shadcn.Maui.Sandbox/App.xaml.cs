d6012916dbb4b516b37a6757acd7c8da510c1769
osmanaslancan <osmanaslancan@users.noreply.github.com>
2024-08-26 13:23:31 +0300

    update maui and community toolkit maui versions

--- Shadcn.Maui.Sandbox/App.xaml.cs
+++ Shadcn.Maui.Sandbox/App.xaml.cs
@@ -1,12 +1,16 @@
-﻿namespace Shadcn.Maui.Sandbox
+﻿
+namespace Shadcn.Maui.Sandbox
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+        }
 
-            MainPage = new AppShell();
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
         }
     }
 }
